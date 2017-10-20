3009982ee6abf6c8f2a2c633ee0b5e9e592d698c
Silver Ibenye <silver.ibenye@forrent.com>
2017-10-20 19:03:21 -0400

    Delete e2e.properties

--- src/test/endToEnd/java/com/elsynergy/nigerianpostcodes/controller/GeographyControllerTest.java
+++ src/test/endToEnd/java/com/elsynergy/nigerianpostcodes/controller/GeographyControllerTest.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,7 +24,6 @@ import com.elsynergy.nigerianpostcodes.util.QueryUriHelper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class)
-@TestPropertySource(locations = "classpath:e2etest.properties")
 @WebAppConfiguration
 public class GeographyControllerTest
 {
